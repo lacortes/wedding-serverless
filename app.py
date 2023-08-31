0dd2d67c1cb1f6d1179563c2beb7983766abf343
Luis Cortes <cortes72luis@gmail.com>
2023-08-30 21:37:36 -0700

    feat: update backend

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import datetime
 import enum
-from typing import Annotated, Optional
+from typing import Annotated, Optional, List
 
 from fastapi import FastAPI, Query, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
-from pydantic import BaseModel, BaseSettings
+from pydantic import BaseModel, BaseSettings, conint, root_validator
 from sqlalchemy.engine import URL
 from sqlmodel import create_engine, SQLModel, Session, select, col, Field
 
@@ -46,7 +46,19 @@ class Guest(SQLModel, table=True):
     guest_type: str
     avail_guests: int
     rsvp: str
+    selection: int
     updated_at: datetime.datetime
+
+
+class SecondaryGuest(SQLModel, table=True):
+    __tablename__ = 'secondary_guest'
+
+    guest_id: Optional[int] = Field(default=None, primary_key=True)
+    primary_guest_id: Optional[int] = Field(default=None, foreign_key="guest.guest_id")
+    first_name: str
+    last_name: str
+    rsvp: str
+    selection: int
 
 
 class GuestUpdate(BaseModel):
@@ -55,9 +67,25 @@ class GuestUpdate(BaseModel):
     rsvp: RSVP
 
 
-@app.get("/")
-async def get_settings():
-    return {"message": settings.dict()}
+class RsvpGuest(BaseModel):
+    first_name: Annotated[str, Field(min_length=3, max_length=20, regex="[a-zA-Z]")]
+    last_name: Annotated[str, Field(min_length=3, max_length=20, regex="[a-zA-Z]")]
+    rsvp: RSVP
+    selection: conint(ge=0, le=2)
+
+    @root_validator
+    def check_selection(cls, values):
+        rsvp, selection = values.get('rsvp'), values.get('selection')
+        if rsvp == RSVP.not_attending and selection != 0:
+            raise ValueError('Cannot make a selection if not attending')
+        if rsvp == RSVP.attending and selection == 0:
+            raise ValueError('Cannot attend and have no selection')
+        return values
+
+
+class RsvpCreate(BaseModel):
+    guest: RsvpGuest
+    party: List[RsvpGuest]
 
 
 @app.get("/v1/guests", response_model=Guest)
@@ -84,6 +112,35 @@ async def update_guest(guest: GuestUpdate):
     return guest_db
 
 
+@app.post("/v1/guests/rsvp")
+async def create_rsvp(rsvp: RsvpCreate):
+    guest_db = get_guest_db(rsvp.guest.first_name, rsvp.guest.last_name)
+    if guest_db is None:
+        raise HTTPException(status_code=404, detail="Primary Guest not found")
+
+    if guest_db.rsvp != RSVP.pending or rsvp.guest.rsvp == guest_db.rsvp:
+        raise HTTPException(status_code=409, detail="Cannot create rsvp for an already created rsvp")
+
+    if rsvp.guest.rsvp == RSVP.attending and guest_db.avail_guests <= 0 < len(
+            rsvp.party) or guest_db.avail_guests < len(rsvp.party):
+        raise HTTPException(
+            status_code=400,
+            detail=f"{guest_db.first_name} {guest_db.last_name} has disallowed guests."
+        )
+
+    guest_db.avail_guests -= len(rsvp.party)
+    guest_db.selection = rsvp.guest.selection
+    guest_db.rsvp = rsvp.guest.rsvp
+    guest_db.updated_at = datetime.datetime.now()
+    update_guest_db(guest_db)
+
+    if len(rsvp.party) > 0:
+        guests = [SecondaryGuest(**g.dict(), primary_guest_id=guest_db.guest_id) for g in rsvp.party]
+        add_secondary_guests(guests)
+
+    return guest_db
+
+
 engine = create_engine(
     URL.create(
         drivername="postgresql",
@@ -95,6 +152,14 @@ engine = create_engine(
     ),
     echo=True
 )
+
+
+def add_secondary_guests(guests: list[SecondaryGuest]):
+    with Session(engine) as session:
+        for guest in guests:
+            session.add(guest)
+
+        session.commit()
 
 
 def get_guest_db(first_name: str, last_name: str):
--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,28 @@
 CREATE TYPE attendance as ENUM ('ATTENDING', 'NOT_ATTENDING', 'PENDING');
 CREATE TYPE guest_type as ENUM ('PRIMARY', 'SECONDARY');
-CREATE TABLE guest(
-    guest_id  SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    guest_type guest_type NOT NULL DEFAULT 'PRIMARY',
-    rsvp attendance NOT NULL DEFAULT 'PENDING',
-    avail_guests INTEGER NOT NULL DEFAULT 0,
-    created_at timestamp NOT NULL DEFAULT NOW(),
-    updated_at timestamp NOT NULL DEFAULT NOW()
+CREATE TABLE guest
+(
+    guest_id     SERIAL PRIMARY KEY,
+    first_name   VARCHAR(50) NOT NULL,
+    last_name    VARCHAR(50) NOT NULL,
+    guest_type   guest_type  NOT NULL DEFAULT 'PRIMARY',
+    rsvp         attendance  NOT NULL DEFAULT 'PENDING',
+    selection    smallint    NOT NULL DEFAULT 0 CHECK ( selection >= 0 AND selection <= 2),
+    avail_guests INTEGER     NOT NULL DEFAULT 0,
+    created_at   timestamp   NOT NULL DEFAULT NOW(),
+    updated_at   timestamp   NOT NULL DEFAULT NOW()
 );
+CREATE INDEX idx_fullname ON guest (first_name, last_name);
+
+CREATE TABLE secondary_guest
+(
+    guest_id         SERIAL PRIMARY KEY,
+    primary_guest_id INT REFERENCES guest (guest_id) ON DELETE CASCADE,
+    first_name       VARCHAR(50) NOT NULL,
+    last_name        VARCHAR(50) NOT NULL,
+    rsvp             attendance  NOT NULL DEFAULT 'PENDING',
+    selection        smallint    NOT NULL DEFAULT 0 CHECK ( selection >= 0 AND selection <= 2),
+    created_at       timestamp   NOT NULL DEFAULT NOW(),
+    updated_at       timestamp   NOT NULL DEFAULT NOW()
+);
+CREATE INDEX idx_fullname_secondary ON secondary_guest (first_name, last_name);
